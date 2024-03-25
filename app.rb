9ef9f296207f302d4eb50ca8c87a0fbf418ed96d
Matteo Restuccia <102557721+mrestuccia1@users.noreply.github.com>
2024-03-25 20:51:07 +0000

    done

--- app.rb
+++ app.rb
@@ -23,13 +23,13 @@ end
 
 get("/square/results") do
   @num = params['user_number'].to_f
-  @result = (@num**2).round(2)
+  @result = (@num**2).round(4)
   erb(:square_res)
 end
 
 get("/square_root/results") do
   @num = params['user_number'].to_f
-  @result = Math.sqrt(@num).round(2)
+  @result = Math.sqrt(@num).round(4)
   erb(:square_root_res)
 end
 
@@ -38,17 +38,17 @@ get("/random/results") do
   @mini = params['user_min'].to_f
   @maxi = params['user_max'].to_f
 
-  @result = (@mini + rand * (@maxi - @mini)).round(2)
+  @result = (@mini + rand * (@maxi - @mini)).round(4)
   erb(:random_res)
 
 end
 
 get("/payment/results") do
   @apr = params["user_apr"].to_f
-  @years = params["user_years"].to_i
+  @years = params["user_years"].to_f
   @present_value = params["user_pv"].to_f
 
   monthly_rate = @apr / 100 / 12
-  @result = @present_value * (monthly_rate / (1 - (1 + monthly_rate) ** (-@years * 12))).round(2)
+  @result = @present_value * (monthly_rate / (1 - (1 + monthly_rate) ** (-@years * 12)))
   erb(:payment_res)
 end
