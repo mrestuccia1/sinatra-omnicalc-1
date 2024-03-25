77f521682487601e8ca33e91330289c8d09b37b1
Matteo Restuccia <102557721+mrestuccia1@users.noreply.github.com>
2024-03-25 20:32:08 +0000

    done

--- app.rb
+++ app.rb
@@ -2,8 +2,53 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/square/results") do
+  @num = params['user_number'].to_f
+  @result = (@num**2).round(2)
+  erb(:square_res)
+end
+
+get("/square_root/results") do
+  @num = params['user_number'].to_f
+  @result = Math.sqrt(@num).round(2)
+  erb(:square_root_res)
+end
+
+get("/random/results") do
+  
+  @mini = params['user_min'].to_f
+  @maxi = params['user_max'].to_f
+
+  @result = (@mini + rand * (@maxi - @mini)).round(2)
+  erb(:random_res)
+
+end
+
+get("/payment/results") do
+  @apr = params["user_apr"].to_f
+  @years = params["user_years"].to_i
+  @present_value = params["user_pv"].to_f
+
+  monthly_rate = @apr / 100 / 12
+  @result = @present_value * (monthly_rate / (1 - (1 + monthly_rate) ** (-@years * 12))).round(2)
+  erb(:payment_res)
 end
